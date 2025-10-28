a6abb2e82a84ad384ceb85e26652409ff5ff4aa4
Purab2001 <a.s.purab0@gmail.com>
2025-10-29 01:25:16 +0600

    Refactor Inngest client initialization and improve API route formatting

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,11 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
-import { deleteCouponOnExpiry, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/inngest/functions";
+import {
+  deleteCouponOnExpiry,
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdation,
+} from "@/inngest/functions";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
@@ -11,4 +16,5 @@ export const { GET, POST, PUT } = serve({
     syncUserDeletion,
     deleteCouponOnExpiry,
   ],
+  signingKey: process.env.INNGEST_SIGNING_KEY,
 });
